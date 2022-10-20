743d092ae8bbaf3a02cb581714af7dfc9e457c5f
khriemer <61724999+khriemer2596@users.noreply.github.com>
2022-10-20 14:47:13 -0500

    Update random.js

--- Random User Generator Web App/public/random.js
+++ Random User Generator Web App/public/random.js
@@ -1,14 +1,5 @@
 'use strict'
 
-/* const { response } = require("express") */
-
-/* Write functions to: 
-   Return the display of the string for a random person,
-   Create a node and display the string that represents the person.
-   Asynchronously handle the event.
-   Add an event listener for the buttons.
-*/
-
 function userString(person) {
 
    return (`
