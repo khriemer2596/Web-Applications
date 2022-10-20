604455879ddabc01a0512b5f32174ff2d4be8132
khriemer <61724999+khriemer2596@users.noreply.github.com>
2022-10-20 14:44:33 -0500

    Add files via upload

---
+++ Random User Generator Web App/public/random.js
@@ -0,0 +1,54 @@
+'use strict'
+
+/* const { response } = require("express") */
+
+/* Write functions to: 
+   Return the display of the string for a random person,
+   Create a node and display the string that represents the person.
+   Asynchronously handle the event.
+   Add an event listener for the buttons.
+*/
+
+function userString(person) {
+
+   return (`
+   Name: ${person.name.first}
+   ${person.name.last},
+   Phone Number: ${person.phone},
+   Email: ${person.email}`)
+
+}
+
+function personData(person) {
+   const pData = document.createElement('li')
+   pData.textContent = userString(person)
+   const ranData = document.getElementById('resultingData')
+   ranData.appendChild(pData)
+}
+
+async function getRandomData(object) {
+   object.preventDefault()
+   const targetID = object.target.getAttribute('id')
+   const link = targetID === 'fromTheBrowser' ? "https://randomuser.me/api": "/random-person"
+
+   try {
+      const res = await fetch(link)
+      const data = await res.json()
+
+      if (res.status == 200) {
+         personData(data.results[0])
+      }
+   }
+
+   catch (error) {
+      console.error(error)
+   }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+   const broswerURL = document.getElementById('fromTheBrowser')
+   broswerURL.addEventListener('click', getRandomData)
+
+   const serverURL = document.getElementById('fromTheServer')
+   serverURL.addEventListener('click', getRandomData)
+})
